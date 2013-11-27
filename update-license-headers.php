25bc54443005fedfe77ca2d9c4441d85e4ccdf7d
Nima Dehnashi <ndehnashi@sugarcrm.com>
2013-11-27 15:45:00 -0800

    Initial commit of license header script.

---
+++ update-license-headers.php
@@ -0,0 +1,141 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * Copyright (c) 2013 Nima Dehnashi
+ * https://github.com/ndehnashi/update-license-headers
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @file
+ * This script helps in updating the license headers for SugarCRM 7.0.
+ */
+
+if (version_compare(PHP_VERSION, '5.3.', '<')) {
+    throw new \RuntimeException("This script requires php 5.3.0 version or above.");
+}
+
+$headerText = '/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright (C) 2004-2013 SugarCRM Inc. All rights reserved.
+ */';
+$commentBlockMap = array(
+    "hbs" => array(
+        "start_tok" => "{{!",
+        "end_tok" => "}}",
+    ),
+    "default" => array(
+        "start_tok" => "/*",
+        "end_tok" => "*/",
+    ),
+);
+
+// 'd' = dir, 'e' = file extensions
+$shortOpts = "d::e::";
+$longOpts = array(
+    "dir::",
+    "ext::"
+);
+
+$opts = getopt($shortOpts, $longOpts);
+
+// This option specifies what directory of files to target.
+// e.g. -f=clients/base/views will only update license headers in the views folder.
+$dir = getcwd();
+if (!empty($opts['d'])) {
+    $dir = $opts['d'];
+} else if (!empty($opts['dir'])) {
+    $dir = $opts['dir'];
+}
+
+// This option specifies what type of files (extensions) to target.
+// e.g. -e=hbs will only update license headers on hbs files.
+$type = 'all';
+if (!empty($opts['e'])) {
+    $type = $opts['e'];
+} else if (!empty($opts['ext'])) {
+    $type = $opts['ext'];
+}
+
+$rdi = new RecursiveDirectoryIterator($dir);
+foreach(new RecursiveIteratorIterator($rdi) as $file) {
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+
+    if ($type === 'all' || $ext === $type) {
+        $str = file_get_contents($file);
+        $len = strlen($str);
+        $stack = array();
+        $final = array();
+
+        if (!array_key_exists($ext, $commentBlockMap)) {
+            $ext = 'default';
+        }
+
+        $startTokLen = strlen($commentBlockMap[$ext]['start_tok']);
+        $endTokLen = strlen($commentBlockMap[$ext]['end_tok']);
+
+        for($pos = 0; $pos < $len; $pos++) {
+            $isStartTok = substr($str, $pos, $startTokLen);
+            $isEndTok = substr($str, $pos, $endTokLen);
+
+            if ($isStartTok === $commentBlockMap[$ext]['start_tok']) {
+                array_push($stack, $pos);
+            } else if ($isEndTok === $commentBlockMap[$ext]['end_tok']) {
+                if (count($stack) === 1) {
+                    $final = array(
+                        'start' => reset($stack),
+                        'end' => $pos
+                    );
+                    break;
+                } else {
+                    array_pop($stack);
+                }
+            }
+        }
+
+        if (!empty($final)) {
+            $header = $headerText;
+            if ($ext !== 'default') {
+                if ($ext === 'hbs') {
+                    // Ultimately, we want to have the "{{!--" token used in hbs
+                    // license headers regardless of whether "{{!--" or "{{!" was used.
+                    // This slight hack is OK because "{{!" is a subset of "{{!--".
+                    $startTok = "{{!--";
+                    $endTok = "--}}";
+                    $header = $startTok . "\n" . $headerText . "\n" . $endTok;
+                } else {
+                    $header = $commentBlockMap[$ext]['start_tok'] . "\n" . $headerText . "\n" .
+                        $commentBlockMap[$ext]['end_tok'];
+                }
+            }
+
+            $commentLength = $final['end'] - $final['start'] + $endTokLen;
+            $comment = substr($str, $final['start'], $commentLength);
+            if (strcmp($header, $comment) !== 0) {
+                // Check if this is in fact a license header comment.
+                if (stripos($comment, 'all rights reserved') !== false) {
+                    $str = str_replace($comment, $header, $str);
+                    file_put_contents($file, $str, LOCK_EX);
+                    echo "{$file} ✓\n";
+                } else {
+                    echo "Comment found. No license headers found in {$file}\n";
+                }
+            } else {
+                echo "License header up to date in {$file}\n";
+            }
+        } else {
+            echo "No license headers found in {$file}\n";
+        }
+    }
+}
