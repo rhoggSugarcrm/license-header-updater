8c92b200dba80706488ddcb64f7f66568d462df7
Nima Dehnashi <ndehnashi@sugarcrm.com>
2013-12-16 11:55:56 -0800

    Added LicenseUpdater class, cleaned up code.

--- update-license-headers.php
+++ update-license-headers.php
@@ -23,7 +23,61 @@ if (!defined('sugarEntry')) {
     define('sugarEntry', true);
 }
 
-$headerText = '/*
+process($argv);
+
+function process($argv) {
+    // 'd' = dir, 'e' = file extensions
+    $shortOpts = "d::e::";
+    $longOpts = array(
+        "dir::",
+        "ext::"
+    );
+
+    $opts = getopt($shortOpts, $longOpts);
+
+    // This option specifies what directory of files to target.
+    // e.g. -f=clients/base/views will only update license headers in the views folder.
+    $dir = getcwd();
+    if (!empty($opts['d'])) {
+        $dir = $opts['d'];
+    } else if (!empty($opts['dir'])) {
+        $dir = $opts['dir'];
+    }
+
+    // This option specifies what type of files (extensions) to target.
+    // e.g. -e=hbs will only update license headers on hbs files.
+    $type = 'all';
+    if (!empty($opts['e'])) {
+        $type = $opts['e'];
+    } else if (!empty($opts['ext'])) {
+        $type = $opts['ext'];
+    }
+
+    $updater = new LicenseUpdater();
+    $rdi = new RecursiveDirectoryIterator($dir);
+    $filter = new CustomRecursiveFilterIterator($rdi);
+    $files = new RecursiveIteratorIterator($filter);
+
+    foreach($files as $file) {
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+        if ($type === 'all' || $ext === $type) {
+            $updater->setFileExt($ext);
+            $updater->run($file, $ext);
+        }
+    }
+}
+
+class LicenseUpdater
+{
+    protected $fileExt = '';
+
+    protected $fileContent = '';
+
+    // We can't insert license headers in these types of files.
+    protected $blacklist = array('json');
+
+    protected $headerText = <<<'EOT'
+/*
  * By installing or using this file, you are confirming on behalf of the entity
  * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
  * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
@@ -34,70 +88,51 @@ $headerText = '/*
  * certifying that you have authority to bind Company accordingly.
  *
  * Copyright (C) 2004-2013 SugarCRM Inc. All rights reserved.
- */';
-$commentBlockMap = array(
-    "hbs" => array(
-        "start_tok" => "{{!",
-        "end_tok" => "}}",
-    ),
-    "html" => array(
-        "start_tok" => "<!--",
-        "end_tok" => "-->",
-    ),
-    "tpl" => array(
-        "start_tok" => "<!--",
-        "end_tok" => "-->",
-    ),
-    "default" => array(
-        "start_tok" => "/*",
-        "end_tok" => "*/",
-    ),
-);
+ */
+EOT;
 
-// 'd' = dir, 'e' = file extensions
-$shortOpts = "d::e::";
-$longOpts = array(
-    "dir::",
-    "ext::"
-);
+    protected $commentBlockMap = array(
+        'hbs' => array(
+            'start_tok' => '{{!',
+            'end_tok' => '}}',
+        ),
+        'html' => array(
+            'start_tok' => '<!--',
+            'end_tok' => '-->',
+        ),
+        'tpl' => array(
+            'start_tok' => '<!--',
+            'end_tok' => '-->',
+        ),
+        'default' => array(
+            'start_tok' => '/*',
+            'end_tok' => '*/',
+        ),
+    );
 
-$opts = getopt($shortOpts, $longOpts);
+    // These files require tokens preceding the license header itself.
+    protected $prependTokenMap = array(
+        'php' => '<?php'
+    );
 
-// This option specifies what directory of files to target.
-// e.g. -f=clients/base/views will only update license headers in the views folder.
-$dir = getcwd();
-if (!empty($opts['d'])) {
-    $dir = $opts['d'];
-} else if (!empty($opts['dir'])) {
-    $dir = $opts['dir'];
-}
+    public function setFileExt($ext)
+    {
+        $this->fileExt = $ext;
+    }
 
-// This option specifies what type of files (extensions) to target.
-// e.g. -e=hbs will only update license headers on hbs files.
-$type = 'all';
-if (!empty($opts['e'])) {
-    $type = $opts['e'];
-} else if (!empty($opts['ext'])) {
-    $type = $opts['ext'];
-}
-
-$rdi = new RecursiveDirectoryIterator($dir);
-foreach(new RecursiveIteratorIterator($rdi) as $file) {
-    $fileExt = pathinfo($file, PATHINFO_EXTENSION);
-    $ext = $fileExt;
-
-    if ($type === 'all' || $ext === $type) {
-        $str = file_get_contents($file);
-        $len = strlen($str);
+    public function run($file, $ext)
+    {
+        $this->fileContent = file_get_contents($file);
+        $len = strlen($this->fileContent);
         $stack = array();
         $final = array();
 
-        if (!array_key_exists($ext, $commentBlockMap)) {
+        if (!array_key_exists($ext, $this->commentBlockMap)) {
             $ext = 'default';
         }
 
         // Generate an appropriate header for the file.
-        $header = $headerText;
+        $header = $this->headerText;
         if ($ext !== 'default') {
             if ($ext === 'hbs') {
                 // Ultimately, we want to have the "{{!--" token used in hbs
@@ -105,23 +140,23 @@ foreach(new RecursiveIteratorIterator($rdi) as $file) {
                 // This slight hack is OK because "{{!" is a subset of "{{!--".
                 $startTok = "{{!--";
                 $endTok = "--}}";
-                $header = $startTok . "\n" . $headerText . "\n" . $endTok;
+                $header = $startTok . "\n" . $this->headerText . "\n" . $endTok;
             } else {
-                $header = $commentBlockMap[$ext]['start_tok'] . "\n" . $headerText . "\n" .
-                    $commentBlockMap[$ext]['end_tok'];
+                $header = $this->commentBlockMap[$ext]['start_tok'] . "\n" . $this->headerText . "\n" .
+                    $this->commentBlockMap[$ext]['end_tok'];
             }
         }
 
-        $startTokLen = strlen($commentBlockMap[$ext]['start_tok']);
-        $endTokLen = strlen($commentBlockMap[$ext]['end_tok']);
+        $startTokLen = strlen($this->commentBlockMap[$ext]['start_tok']);
+        $endTokLen = strlen($this->commentBlockMap[$ext]['end_tok']);
 
         for($pos = 0; $pos < $len; $pos++) {
-            $isStartTok = substr($str, $pos, $startTokLen);
-            $isEndTok = substr($str, $pos, $endTokLen);
+            $isStartTok = substr($this->fileContent, $pos, $startTokLen);
+            $isEndTok = substr($this->fileContent, $pos, $endTokLen);
 
-            if ($isStartTok === $commentBlockMap[$ext]['start_tok']) {
+            if ($isStartTok === $this->commentBlockMap[$ext]['start_tok']) {
                 array_push($stack, $pos);
-            } else if ($isEndTok === $commentBlockMap[$ext]['end_tok']) {
+            } else if ($isEndTok === $this->commentBlockMap[$ext]['end_tok']) {
                 if (count($stack) === 1) {
                     $final = array(
                         'start' => reset($stack),
@@ -136,44 +171,53 @@ foreach(new RecursiveIteratorIterator($rdi) as $file) {
 
         if (!empty($final)) {
             $commentLength = $final['end'] - $final['start'] + $endTokLen;
-            $comment = substr($str, $final['start'], $commentLength);
+            $comment = substr($this->fileContent, $final['start'], $commentLength);
             if (strcmp($header, $comment) !== 0) {
                 // Check if this is in fact a license header comment.
                 if (stripos($comment, 'all rights reserved') !== false) {
-                    $str = str_replace($comment, $header, $str);
-                    file_put_contents($file, $str, LOCK_EX);
+                    $this->fileContent = str_replace($comment, $header, $this->fileContent);
+                    file_put_contents($file, $this->fileContent, LOCK_EX);
                     echo "{$file} ✓\n";
                 } else {
                     echo "Comment found. No license headers found in {$file}, inserting new license header\n";
-                    insertHeader($file, $str, $fileExt, $header);
+                    $this->insertHeader($file, $header);
                 }
             } else {
                 echo "License header up to date in {$file}\n";
             }
         } else {
             echo "No license headers found in {$file}, inserting new license header\n";
-            insertHeader($file, $str, $fileExt, $header);
+            $this->insertHeader($file, $header);
+        }
+    }
+
+    protected function insertHeader($file, $licenseHeader)
+    {
+        if (!in_array($this->fileExt, $this->blacklist)) {
+            if (array_key_exists($this->fileExt, $this->prependTokenMap)) {
+                // We need to prepend the token to the license header first.
+                $licenseHeader = $this->prependTokenMap[$this->fileExt] . "\n" . $licenseHeader;
+                $this->fileContent = str_replace($this->prependTokenMap[$this->fileExt], $licenseHeader, $this->fileContent);
+            } else {
+                $this->fileContent = $licenseHeader . "\n" . $this->fileContent;
+            }
+            file_put_contents($file, $this->fileContent, LOCK_EX);
         }
     }
 }
 
-function insertHeader($file, $str, $fileType, $licenseHeader)
+class CustomRecursiveFilterIterator extends RecursiveFilterIterator
 {
-    // We can't insert license headers in these types of files.
-    $blacklist = array('json');
-    // These files require tokens preceding the license header itself.
-    $prependTokenMap = array(
-        "php" => "<?php"
-    );
+    public $rejected = array();
 
-    if (!in_array($fileType, $blacklist)) {
-        if (array_key_exists($fileType, $prependTokenMap)) {
-            // We need to prepend the token to the license header first.
-            $licenseHeader = $prependTokenMap[$fileType] . "\n" . $licenseHeader;
-            $str = str_replace($prependTokenMap[$fileType], $licenseHeader, $str);
-        } else {
-            $str = $licenseHeader . "\n" . $str;
+    public function accept()
+    {
+        $filename = $this->current()->getFilename();
+        $isValid = !fnmatch('.*', $filename);
+
+        if ($isValid === false) {
+            $this->rejected[] = $filename;
         }
-        file_put_contents($file, $str, LOCK_EX);
+        return $isValid;
     }
 }
